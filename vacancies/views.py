303627f6dffdad9c3f55561ce337c822b9888b2f
San <andreal87@mail.ru>
2022-12-02 15:08:22 +0600

    init project

---
+++ vacancies/views.py
@@ -0,0 +1,51 @@
+import json
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
+
+from vacancies.models import Vacancy
+
+
+def hello(request):
+    return HttpResponse("Hello, World!")
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class VacancyView(View):
+    def get(self, request):
+        vacancies = Vacancy.objects.all()
+        search_text = request.GET.get("text")
+        if search_text:
+            vacancies = vacancies.filter(text=search_text)
+        response = []
+        for vacancy in vacancies:
+            response.append({
+                "id": vacancy.id,
+                "text": vacancy.text,
+            })
+        return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False})
+
+    def post(self, request):
+        vacancy_data = json.loads(request.body)
+        vacancy = Vacancy()
+        vacancy.text = vacancy_data["text"]
+        vacancy.save()
+        return JsonResponse({
+            "id": vacancy.id,
+            "text": vacancy.text,
+        })
+
+
+class VacancyDetailView(DetailView):
+    model = Vacancy
+
+    def get(self, request, *args, **kwargs):
+        vacancy = self.get_object()
+        return JsonResponse({
+            "id": vacancy.id,
+            "text": vacancy.text,
+        })
