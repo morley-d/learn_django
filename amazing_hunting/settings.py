ca51ac9bd51f726f1a1256e4667e339815645c15
San <andreal87@mail.ru>
2022-12-12 16:51:35 +0600

    serializers

--- amazing_hunting/settings.py
+++ amazing_hunting/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'vacancies',
     'companies',
 ]
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+TOTAL_ON_PAGE = 10
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.paginator import Paginator
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
@@ -7,7 +8,9 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
+from amazing_hunting import settings
 from vacancies.models import Vacancy, Skill
+from vacancies.serializers import VacancySerializer
 
 
 def hello(request):
@@ -21,15 +24,18 @@ class VacancyListView(ListView):
     def get(self, request, *args, **kwargs):
         super().get(request, *args, **kwargs)
 
-        search_text = request.GET.get("text")
+        search_text = request.GET.get("text", None)
+        self.object_list = self.object_list.select_related("user").prefetch_related("skills").order_by("text")
         if search_text:
             self.object_list = self.object_list.filter(text=search_text)
-        response = []
-        for vacancy in self.object_list:
-            response.append({
-                "id": vacancy.id,
-                "text": vacancy.text,
-            })
+        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        response = {
+            "items": VacancySerializer(page_obj, many=True).data,
+        }
+
         return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False})
 
 
